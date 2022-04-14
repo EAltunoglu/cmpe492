f675e116c82dc256f74d5df2dfba23d85ae74f6b
Ekrem <ekremyusufekmekci@gmail.com>
2022-04-14 09:42:56 +0300

    initial structure

---
+++ scripts/displot/displot.py
@@ -0,0 +1,17 @@
+import seaborn as sns
+import pandas as pd
+# import matplotlib.pyplot as plt
+
+def display_displo(data: pd.DataFrame):
+    print('start displaying')
+    print()
+    for column in data.columns:
+        print('asd')
+        # sns.displot(data, x=column)
+    print()
+
+# sleeps = pd.read_csv('original_data\sleep.csv')
+
+
+# display_displot(sleeps)
+
