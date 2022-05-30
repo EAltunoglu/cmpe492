16c5f79b6dfda3c43f1cc5732efe61900fb71b80
EAltunoglu <erenaltunoglu@gmail.com>
2022-05-30 15:02:02 +0300

    meanrate regressor

--- date_class.py
+++ date_class.py
@@ -20,55 +20,51 @@ for row in calenderrows:
 
 def get_class(date: str):
     date_float = float(date.replace('-', ''))
-    next_class_begin = None
-    last_class_begin = 20140325
-    last_final_exam = 20140325
-    next_final_exam = None
+    # next_class_begin = None
+    # last_class_begin = 20140325
+    # last_final_exam = 20140325
+    # next_final_exam = None
     last_applicable = 'Summer break'
 
     for row in shortenedrows:
         if row[BEGIN_DATE] <= date_float <= row[END_DATE]:
             if row['Event'] == 'Class begins' or row['Event'] == 'Classes begin':
-                return 'Class'
-            return row['Event']
+                return 'class'
+            if row['Event'] == 'Final exam':
+                return 'final exams'
+            return row['Event'].lower()
         
-        if row[BEGIN_DATE] <= date_float and math.isnan:
-            last_applicable = row['Event']
+        # if row[BEGIN_DATE] <= date_float and math.isnan:
+        #     last_applicable = row['Event']
 
-        if row[END_DATE] < date_float and row['Event'] == 'Final exams':
-            last_final_exam = row[END_DATE]
+        # if row[END_DATE] < date_float and row['Event'] == 'Final exams':
+        #     last_final_exam = row[END_DATE]
         
-        if row[BEGIN_DATE] > date_float and next_class_begin is None and row['Event'] == 'Class begins':
-            next_class_begin = row[BEGIN_DATE]
+        # if row[BEGIN_DATE] > date_float and next_class_begin is None and row['Event'] == 'Class begins':
+        #     next_class_begin = row[BEGIN_DATE]
         
-        if row[BEGIN_DATE] > date_float and next_final_exam is None and row['Event'] == 'Final Exams':
-            next_class_begin = row[BEGIN_DATE]
+        # if row[BEGIN_DATE] > date_float and next_final_exam is None and row['Event'] == 'Final Exams':
+        #     next_class_begin = row[BEGIN_DATE]
         
         # if row[]
 
-    if not next_class_begin:
-        next_class_begin = 20220000
-    if not next_final_exam:
-        next_final_examp = 2022000
-
-    # if last_final_exam <= date_float  <= next_class_begin:
-    #     if not (2 <= int(date.split('-')[1]) <= 10):
-    #         return 'Winter break'
-        
-    #     return 'Summer break'
+    # if not next_class_begin:
+    #     next_class_begin = 20220000
+    # if not next_final_exam:
+    #     next_final_examp = 2022000
 
     if last_applicable == 'Class begins' or last_applicable == 'Classes begin':
         last_applicable = 'Class'
 
     if last_applicable in holidays:
-        return 'Class'
+        return 'class'
     
-    if last_applicable == 'Final exams':
+    if last_applicable == 'Final exams' or last_applicable == 'Final exam':
         if 2 <= int(date.split('-')[1]) <= 10:
-            return 'Summer break'
-        return 'Winter break'
+            return 'summer break'
+        return 'winter break'
 
-    return last_applicable
+    return last_applicable.lower()
 
 
 # print(get_class('2015-08-22'))
@@ -86,7 +82,6 @@ def get_class(date: str):
 # print(get_class('2015-11-30'))
 # print(get_class('2015-12-18'))
 # print(get_class('2015-12-19'))
-
 
 
 activity = pd.read_csv('original_data/activity.csv')
