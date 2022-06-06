c59a814823eec8d89d536c26c70d2d1321115b73
EAltunoglu <erenaltunoglu@gmail.com>
2022-06-06 17:45:50 +0300

    Random forest classifier multiple cases

--- date_class.py
+++ date_class.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import math
 
 calendercsv = pd.read_csv('original_data/calender.csv')
@@ -7,44 +6,45 @@ calenderrows = [row for index, row in calendercsv.iterrows()]
 shortenedrows = []
 BEGIN_DATE = 'Beginning event date'
 END_DATE = 'End event date'
-# BEGIN_DATE = 'Begin Date'
-# END_DATE = 'End date'
-holidays = ['Thanksgiving', 'Mid-term break', 'Mid-Term break', 'Easter', 'Halloween', 'Christmas', 'Fall break', 'Spring break', 'Summer break', 'Winter break', 'fall break', 'spring break', 'summer break', 'winter break', 'thanksgiving', 'easter', 'halloween']
+holidays = ['Thanksgiving', 'Mid-term break', 'Mid-Term break', 'Easter', 'Summer break', 'Winter break', 'Fall break', 'Spring break', 'fall break', 'spring break']
 
 
 for row in calenderrows:
-    # if not math.isnan(row['Beginning event date']) and not math.isnan(row['End event date']):
     if not math.isnan(row[BEGIN_DATE]):
         shortenedrows.append(row)
 
+def get_term(date: str):
+    if 1 <= int(date.split('-')[1]) <= 7:
+        return 'spring'
+    return 'fall'
 
 def get_class(date: str):
     date_float = float(date.replace('-', ''))
-    last_applicable = 'b'
+    last_applicable = 'summer_break'
 
     for row in shortenedrows:
         if row[BEGIN_DATE] <= date_float <= row[END_DATE]:
             if row['Event'] == 'Class begins' or row['Event'] == 'Classes begin':
-                return 'c'
-            if row['Event'] == 'Final exam' or row['Event'] == 'Final exams':
-                return 'f'
-            return 'b'
+                return get_term(date) + '_class'
+            if row['Event'] == 'Final exam' or row['Event'] == 'Final Exams' or row['Event'] == 'Final exams':
+                return get_term(date) + '_final'
+            return get_term(date) + '_break'
         
         if row[BEGIN_DATE] <= date_float:
             last_applicable = row['Event']
 
-
     if last_applicable == 'Class begins' or last_applicable == 'Classes begin':
-        last_applicable = 'c'
+        last_applicable = get_term(date) + '_class'
 
     if last_applicable in holidays:
-        return 'c'
+        return get_term(date) + '_class'
     
     if last_applicable == 'Final exams' or last_applicable == 'Final exam':
-        return 'b'
+        if 2 <= int(date.split('-')[1]) <= 10:
+            return 'summer_break'
+        return 'winter_break'
 
-    return last_applicable
-
+    return last_applicable.lower()
 
 # print(get_class('2015-08-22'))
 # print(get_class('2015-08-23'))
@@ -62,24 +62,35 @@ def get_class(date: str):
 # print(get_class('2015-12-18'))
 # print(get_class('2015-12-19'))
 
+# print(get_class('2017-04-14'))
+
+# print(get_class('2016-01-10'))
+# print(get_class('2016-01-11'))
+# print(get_class('2016-01-12'))
+# print(get_class('2016-01-13'))
 
 
+#### 2015
 # activity = pd.read_csv('original_data/activity.csv')
-# activity['datadate'] = activity['datadate'].apply(get_class)
-# activity.to_csv('original_data/activity_date_class.csv', index=False)
-
 # sleep = pd.read_csv('original_data/sleep_daily.csv')
-# sleep['dataDate'] = sleep['dataDate'].apply(get_class)
-# sleep.to_csv('original_data/sleep_daily_date_class.csv', index=False)
 
+# activity = activity[activity['datadate'].str.startswith("2015")]
+# sleep = sleep[sleep['dataDate'].str.startswith("2015")]
+
+# activity_sleep = pd.merge(sleep, activity, left_on=['egoid', 'dataDate'], right_on=['egoid', 'datadate'] )
+# activity_sleep.drop('datadate', axis=1, inplace=True)
+# activity_sleep['dataDate'] = activity_sleep['dataDate'].apply(get_class)
+# activity_sleep.to_csv('merged_data/activity_sleep_daily_date_extended_class.csv', index=False)
+
+
+#### 2016
 activity = pd.read_csv('original_data/activity.csv')
 sleep = pd.read_csv('original_data/sleep_daily.csv')
 
-activity = activity[activity['datadate'].str.startswith("2015")]
-sleep = sleep[sleep['dataDate'].str.startswith("2015")]
-
+activity = activity[activity['datadate'].str.startswith("2016")]
+sleep = sleep[sleep['dataDate'].str.startswith("2016")]
 
 activity_sleep = pd.merge(sleep, activity, left_on=['egoid', 'dataDate'], right_on=['egoid', 'datadate'] )
 activity_sleep.drop('datadate', axis=1, inplace=True)
 activity_sleep['dataDate'] = activity_sleep['dataDate'].apply(get_class)
-activity_sleep.to_csv('merged_data/activity_sleep_daily_date_class_2015.csv', index=False)
+activity_sleep.to_csv('merged_data/activity_sleep_daily_date_extended_class_2016.csv', index=False)
